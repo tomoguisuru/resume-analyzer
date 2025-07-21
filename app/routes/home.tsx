a391d94c0690e347c4e3b8fb5dc8a9387dd8f716
Brandon Drake <bdrake@edg.io>
2025-07-21 11:05:29 -0400

    Add puter and auth route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "~/constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 
 
@@ -13,6 +16,17 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {isLoading, auth} = usePuterStore();
+  const navigate = useNavigate();
+
+  // Check to see if the user is authenticated, ship off to auth if not
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate('/auth?next=/');
+    }
+  }, [auth.isAuthenticated]);
+
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
